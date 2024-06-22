7f4a194c66cc8a46db72fdc30fc84651ecef698d
Amit Kumar Sharma <amit.official@gmail.com>
2024-06-22 18:41:52 +0530

    v1.0.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='dvarpal',
-    version='0.0.3',
+    version='1.0.0',
     url="https://github.com/sharmaak/dvarpal",
     packages=find_packages(),
     install_requires=[
-        'PyYAML'
-        'requests'
-        'pyotp'
-        'upstox-python-sdk'
+        'PyYAML',
+        'requests',
+        'pyotp',
+        'upstox-python-sdk',
         'undetected_chromedriver'
     ],
     # Include additional metadata like author, author_email, description, etc.
