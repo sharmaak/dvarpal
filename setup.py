eb4c9616bce4bb3f12f6ea451ca8d5f1d15fdf3c
Amit Kumar Sharma <amit.official@gmail.com>
2025-06-13 22:34:16 +0530

    changed config and session file location to $HOME/.dvarpal/

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dvarpal',
-    version='2.0.0',
+    version='2.0.1',
     description='Automated broker login and token generator using Firefox ESR and Selenium',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -26,5 +26,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: POSIX :: Linux",
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.8',
 )
